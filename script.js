75cc7ce883408ef76d4e4473201fae1e5ee36331
riobits <23579928+RioBits@users.noreply.github.com>
2021-06-01 00:32:46 +0300

    better code
    renamed index.js -> script.js & better javascript code

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ const button = document.getElementsByTagName('button')[0]
 button.addEventListener('click', () => {
   const binaryString = document.getElementById('input').value
 
-  if (binaryString.length > 8) {
-    output.value = 'up to 8 binary digits'
-    return
-  }
-
   const notBinaries = binaryString
     .split('')
     .filter((char) => char !== '0' && char !== '1')
 
-  if (notBinaries.length) {
-    output.value = 'Enter binary'
-    return
+  if (notBinaries.length || binaryString === '') {
+    return alert('Please enter binary digits')
   }
 
   const decimal = parseInt(binaryString, 2).toString(10)
